aba4b8f15888f5678b2e2806b29fb63ca73e32df
zzzteph <stephanzag@gmail.com>
2021-11-16 13:35:27 +0100

    poc

---
+++ ssti/lab-java-ssti-attack-1/source/src/test/java/ai/hacktory/mortgage/ApplicationTests.java
@@ -0,0 +1,13 @@
+package ai.hacktory.mortgage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
